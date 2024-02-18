8e12bb4ec991b72f7ca3d42eec8486f51b832bb7
Alexei <alexei.ionov@berkeley.edu>
2024-02-17 17:11:19 -0800

    forgot the black coffeee

--- classes/class_coworkers.py
+++ classes/class_coworkers.py
@@ -4,7 +4,7 @@ class coworkers:
     def __init__(self) -> None:
         self.coworkers_list = [
             coworker("Bob", order("Cappuccino"), 0.0), #default aside from cappucino
-            coworker("Jeremy", order("Regular", False, False, 0), 0.0),  ##black coffee
+            coworker("Jeremy", order("Regular (Black)", False, False, 0), 0.0),  ##black coffee
             coworker("Susan", order(), 0.0), 
             coworker("John", order(), 0.0), 
             coworker("Mary", order(), 0.0), 
